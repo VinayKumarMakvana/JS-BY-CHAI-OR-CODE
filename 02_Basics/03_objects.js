02829dcee1fea73b84313bb27b242dd2f3e323b5
VinayKumarMakvana <vinaytailor52188@gmail.com>
2026-07-19 20:11:10 +0530

    add object practice file

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Vinay",
+    "full name": "Vinay Kumar Makvana",
     [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    age: 19,
+    location: "Kota",
+    email: "vinay@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "vinat@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "vinay@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
